709f1505ceeefcb455b1c970f174234d77dc5b58
mayrars <mayra203@gmail.com>
2024-09-25 05:44:50 -0600

    created patch

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express =  require("express")
 const crypto = require("node:crypto")
 const movies = require("./movies.json")
-const { validateMovie } = require("./schemas/movies")
+const { validateMovie, validatePartialMovie } = require("./schemas/movies")
 const app = express()
 app.use(express.json())  //Middleware para poder recibir json
 
@@ -39,6 +39,30 @@ app.post('/movies',(req, res)=>{
     movies.push(newMovie)
     res.status(201).json(newMovie)
 })
+
+app.patch('/movies/:id', (req, res) => {
+    const result = validatePartialMovie(req.body)
+  
+    if (!result.success) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+  
+    const { id } = req.params
+    const movieIndex = movies.findIndex(movie => movie.id === id)
+  
+    if (movieIndex === -1) {
+      return res.status(404).json({ message: 'Movie not found' })
+    }
+  
+    const updateMovie = {
+      ...movies[movieIndex],
+      ...result.data
+    }
+  
+    movies[movieIndex] = updateMovie
+  
+    return res.json(updateMovie)
+  })
 
 
 const PORT = process.env.PORT || 3000
