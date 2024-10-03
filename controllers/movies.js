882ec60d07f211ae3a0c3ea7bb8a6c2e04dbf2e9
mayrars <mayra203@gmail.com>
2024-10-03 05:57:07 -0600

    Adding js extension

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,4 @@
-import { MovieModel } from "../models/movie"
+import { MovieModel } from "../models/movie.js"
 import {validateMovie, validatePartialMovie} from "../schemas/movies.js"
 export class MovieController{
     static async getAll(req, res){
