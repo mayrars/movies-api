76c36dd15332be3d0d122eae41bfc4ab65241eff
mayrars <mayra203@gmail.com>
2024-10-16 07:51:44 -0600

    Created function createapp

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express, { json } from "express"
-import { moviesRouter } from "./routes/movies.js"
+import { createMovieRouter } from "./routes/movies.js"
 import { corsMiddleware } from "./middlewares/cors.js"
 
-const app = express()
-app.use(json())  //Middleware para poder recibir json
-app.use(corsMiddleware())
-app.disable('x-powered-by')  //Deshabilitando cabecera de express
-
-app.use('/movies', moviesRouter)
-const PORT = process.env.PORT ?? 3000
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+export const createApp = ({movieModel}) => {
+    const app = express()
+    app.use(json())  //Middleware para poder recibir json
+    app.use(corsMiddleware())
+    app.disable('x-powered-by')  //Deshabilitando cabecera de express
+    
+    app.use('/movies', createMovieRouter({movieModel}))
+    const PORT = process.env.PORT ?? 3000
+    
+    app.listen(PORT,()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+}
